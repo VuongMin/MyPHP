abac7d68ce2c3e2adf9835266b7bd49f10bf7847
Nguyễn Minh Vương <vuongm910@gmail.com>
2019-10-04 16:46:32 +0700

    ocean.

---
+++ DigitalOcean/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Digital Ocean</title>
+    <!-- Custom fonts for this template-->
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <script src="bootstrap/lib/jquery-3.4.1.min.js"></script>
+    <script src="bootstrap/lib/popper.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+    <h2>tich hop</h2>
+</div>
+</body>
+</html>
