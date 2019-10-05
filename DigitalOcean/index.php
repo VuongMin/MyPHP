274aa1ee17dcb0c403fc2a41fa91839e3289b0bd
Nguyễn Minh Vương <vuongm910@gmail.com>
2019-10-05 12:29:49 +0700

    frontend.

--- DigitalOcean/index.php
+++ DigitalOcean/index.php
@@ -9,13 +9,265 @@
     <title>Digital Ocean</title>
     <!-- Custom fonts for this template-->
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="scripts/css/custom.css">
     <script src="bootstrap/lib/jquery-3.4.1.min.js"></script>
     <script src="bootstrap/lib/popper.min.js"></script>
     <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body>
-<div class="container">
-    <h2>tich hop</h2>
+<div class="container-fluid">
+    <div class="row nav-top">
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                <p style="line-height: 40px;" >
+                    <span class="btn btn-info" style="    text-transform: uppercase;font-size:11px ;width: 38px;height: 19px;line-height: 6px;padding-left:5px;color: #031b4e;  ">New</span>
+                    <a href="" >
+                        Fresh features and 1-Click Apps for DigitalOcean Kubernetes
+                    </a></p>
+        </div>
+        <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+            <nav  class="header">
+                <ul class="nav justify-content-end">
+
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Docs</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Support</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Sales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link " href="#">Sign in</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+    <hr style="margin: 0;padding: 0"/>
+    <!-- End nav-top -->
+    <div class="row ">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light navbar-top">
+                <a class="navbar-brand" href="#">Digital Ocean</a>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto " >
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                              Products
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Marketplace</a>
+                                <a class="dropdown-item" href="#">Customers</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Marketplace <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Customers</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">Partners
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Community
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Action</a>
+                                <a class="dropdown-item" href="#">Another action</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Something else here</a>
+                            </div>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link " href="#">Pricing
+
+                            </a>
+                        </li>
+
+                    </ul>
+                        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">Sign in</button>
+
+                </div>
+            </nav>
+        </div>
+    </div>
+    <!-- End Navbar--->
+    <hr style="margin: 0;padding: 0"/>
 </div>
+<div class="container">
+    <div class="row ">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 paragraph">
+            <h1 >Get in touch</h1>
+            <p style=" margin: 0 auto;width:900px;text-align: center;font-size: 20px ">Have a question? Send us a note using the form below and someone from the DigitalOcean team will be in touch soon.</p>
+        </div>
+    </div>
+</div>
+<!--End paragraph question. -->
+<div class="container-fluid" style="margin-top: 64px;">
+    <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item" style="width: 386px">
+                    <a class="nav-link active" id="tab-support" data-toggle="tab" href="#home" role="tab" aria-controls="home" aria-selected="true">
+                        Need Support?<br/>
+                        <span  style="color: rgba(3,27,78,.25)">Open a support ticket and get help, fast</span>
+                    </a>
+
+                </li>
+                <li class="nav-item"style="width: 386px">
+                    <a class="nav-link" id="tab-sale" data-toggle="tab" href="#profile" role="tab" aria-controls="profile" aria-selected="false">
+                        Sales Inquiries<br/>
+                        <span class="" style="color: rgba(3,27,78,.25)">Contact sales about large deployments</span>
+                    </a>
+                </li>
+                <li class="nav-item" style="width: 386px">
+                    <a class="nav-link" id="tab-report" data-toggle="tab" href="#contact" role="tab" aria-controls="contact" aria-selected="false">
+                        Report Abuse<br/>
+                        <span class=""style="color: rgba(3,27,78,.25)" >Report any abuse or suspicious activity</span>
+                    </a>
+                </li>
+            </ul>
+            <!--End my tabs-->
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                    <div class="container">
+                        <p class="fast-answer" style="text-align: center">
+                            Existing customer?
+                            <a href="https://cloudsupport.digitalocean.com">Open a ticket</a>.
+                            <br>
+                            Need a fast answer? Search our
+                            <a href="https://www.digitalocean.com/community/questions">Community Q&amp;A</a>.
+                        </p>
+                        <form>
+                            <div class="form-row ">
+                                <div class="form-group col-md-6">
+                                    <input  type="text" class="form-control" id="" placeholder="Name">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="email" class="form-control" id="" placeholder="Email">
+                                </div>
+                            </div>
+                            <div class="form-group">
+                                <select id="inputState" class="form-control">
+                                    <option selected>Reason for getting in touch</option>
+                                    <option>Account / Control Panel</option>
+                                    <option>App / Website</option>
+                                    <option>Billing</option>
+                                    <option>Droplets</option>
+                                    <option>Networking</option>
+                                    <option>Something is Broken</option>
+                                    <option>Spaces</option>
+                                    <option>Volumes</option>
+                                    <option>Other</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Subject">
+                            </div>
+                            <div class="form-row">
+                                <input type="text" class="form-control" id="inputAddress2" placeholder="Brief description of how DigitalOcean can help you">
+                            </div>
+                            <br/>
+                            <button type="submit" class="btn btn-primary">Sign in</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="profile" role="tabpanel" aria-labelledby="profile-tab">
+                    <div class="container">
+                        <form>
+                            <div class="form-row">
+                                <div class="form-group col-md-6">
+                                    <input type="email" class="form-control" id="inputEmail4" placeholder="First Name">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="Last Name">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="email" class="form-control" id="inputEmail4" placeholder="Business Email">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="Business Phone Number">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="Company Name">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="What is your primary role?">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="What is your seniority?">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="What is your use case?">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="What is your estimated monthly spend?">
+                                </div>
+                                <div class="form-group col-md-6">
+                                    <input type="password" class="form-control" id="inputPassword4" placeholder="What is your timeline for deployment/migration?">
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <p>Which products & services are you considering using in this project?</p>
+                                    <input type="checkbox" name="" value="" >Droplets (VMs)<br/>
+                                    <input type="checkbox" name="" value="" >Managed Kubernetes<br/>
+                                    <input type="checkbox" name="" value="" >Spaces Object Storage<br/>
+                                    <input type="checkbox" name="" value="" >Volumes Block Storage<br/>
+                                    <input type="checkbox" name="" value="" >Managed Databases<br/>
+                                    <input type="checkbox" name="" value="" >Premier Support<br/>
+                                    <input type="checkbox" name="" value="" >Networking Tools (LBs, Firewalls, Floating IPs)<br/>
+                                    <input type="checkbox" name="" value="" >Developer Tools (API, Monitoring, Team Accounts)<br/>
+                                </div>
+                                <div class="form-group col-md-12">
+                                    <textarea style="width: 100%" type="" name="" placeholder="What is the biggest challenge you are trying to solve with this project?"></textarea>
+                                </div>
+                            </div>
+
+                                <button style="" type="submit" class="btn btn-primary float-left">Sign in</button>
+
+                        </form>
+                    </div>
+                </div>
+                <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+
+                            </div>
+                            <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
+                                    <p>
+                                        Trademark abuse is the unauthorized use of a trademark on
+                                        or in connection with goods or services in a way that is
+                                        likely to cause confusion about the source of the goods or
+                                        services.
+                                    </p>
+                                    <p>
+                                        Trademarks are national or regionally registered symbols,
+                                        words, signs, or names used to identify or distinguish the
+                                        brand, goods, or services of a company or individual. Each
+                                        country or region has their own specific laws and authorities
+                                        around trademark. The USPTO, the trademark authority in the
+                                        US, requires registration and approval of US recognized
+                                        trademarks.
+                                    </p>
+                                    <p>
+                                        This form provides an easy way for trademark owners (or their
+                                        agents) to notify DigitalOcean about trademark abuse that
+                                        may be hosted by our customers.
+                                    </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
