37128738038cde517d2c14924376338192d5829a
Nguyễn Minh Vương <vuongm910@gmail.com>
2019-10-08 13:51:35 +0700

    tabs!

--- DigitalOcean/core/process.php
+++ DigitalOcean/core/process.php
@@ -1,39 +1,62 @@
 <?php
 require_once 'Validate.php';
 $val= new Validate($_REQUEST);
-$val->addRules(['name'=>['type'=>'string','max'=>100,'min'=>0]])
-    ->addRules(['email'=>['type'=>'email']])
-    ->addRules(['subject'=>['type'=>'string','max'=>100,'min'=>0]])
-    ->addRules(['descripton'=>['type'=>'string','max'=>500,'min'=>0]]);
-
-$val->Run();
+customRulesEveryForm($val,end($_REQUEST));
 session_start();
 $_SESSION['frm_error']=$val->getError();
 //save info to data json
-if(count($val->getError())==0)
+$path_support='../infoJsonData/input';
+$checkSucc= SaveJsonFile($path_support,count($val->getError()));
+$checkSucc ?  $_SESSION['alert_modal']='show':$_SESSION['modal']=null;
+
+//Method process
+function customRulesEveryForm($object,$val_Submit)
 {
-    $path='../infoJsonData/input';
-    if(file_exists($path))
+     if(strtolower($val_Submit)=='sign in')
+     {
+         $object->addRules(['name'=>['type'=>'string','max'=>100,'min'=>0]])
+             ->addRules(['email'=>['type'=>'email']])
+             ->addRules(['subject'=>['type'=>'string','max'=>100,'min'=>0]])
+             ->addRules(['descripton'=>['type'=>'string','max'=>500,'min'=>0]]);
+         $object->Run();
+     }else
+     {
+
+     }
+    if(empty($_REQUEST['reason']))
     {
-        $fp = @fopen($path, "a+");
-        if ($fp) {
-            //so that it is formatted like:[{"name":"Victim","email":"vuongm910@gmail.com","subject":"subject","descripton":"can i help you","submit":"Sign in"}]
-            $data[] = $_REQUEST;
-            $inp = file_get_contents($path);
-            if($inp!=null)
-            {
-                $tempArray = json_decode($inp);
-                array_push($tempArray, $_REQUEST);//format [{},{},{}]
-                $jsonData = json_encode($tempArray);
-                file_put_contents($path, $jsonData);
-            }else
-            {
-                file_put_contents($path, json_encode($data));
-            }
-            //close file
-            fclose($fp);
-        }
+        $object->setError('reason','This value is required.');
     }
+
+}
+function SaveJsonFile($path,$numError)
+{
+    if($numError==0)
+    {
+        if(file_exists($path))
+        {
+            $fp = @fopen($path, "a+");
+            if ($fp) {
+                //so that it is formatted like:[{"name":"Victim","email":"vuongm910@gmail.com","subject":"subject","descripton":"can i help you","submit":"Sign in"}]
+                $data[] = $_REQUEST;
+                $inp = file_get_contents($path);
+                if($inp!=null)
+                {
+                    $tempArray = json_decode($inp);
+                    array_push($tempArray, $_REQUEST);//format [{},{},{}]
+                    $jsonData = json_encode($tempArray);
+                    file_put_contents($path, $jsonData);
+                }else
+                {
+                    file_put_contents($path, json_encode($data));
+                }
+                //close file
+                fclose($fp);
+            }
+        }
+        return true;
+    }
+    return false;
 }
 header('location:../index.php');
 
--- DigitalOcean/scripts/js/custom.js
+++ DigitalOcean/scripts/js/custom.js
@@ -1,12 +1,19 @@
+
 //Event mouse down and leaving
-$('input,textarea').one('blur',function (e) {
-    $('.error li').remove();
-    $('.error').removeAttr('style');
-  if($(this).val()==''||$(this).val()==null)
-  {
-     $(this).parent().attr('style','border-color:red');
-     $(this).before('<li style=" margin-left:13px;font-size:13px ;color: red;font-weight: bold"> This is required</li>');
-  }
+
+$( "input,textarea" ).on({
+    "blur": function() {
+        $('.error li').remove();
+        $('.error').removeAttr('style');
+        if($(this).val()==''||$(this).val()==null) {
+            $(this).parent().attr('style','border-color:red');
+            $(this).before('<li style=" list-style-type: none;margin-left:13px;font-size:13px ;color: red;font-weight: bold"> This is required</li>');
+        }
+    },
+    "keydown": function() {
+        $(this).parent().attr('style','border-color:#0069ff');
+        $('.error li').remove();
+    }
 });
 //Event click post form
 $('button[name="submit"]').off().on('click',function (e) {
@@ -29,3 +36,11 @@ function closeIt()
     }
 }
 window.onbeforeunload = closeIt;
+//refresh tabs
+$('a[data-toggle="tab"]').on('show.bs.tab', function(e) {
+    localStorage.setItem('activeTab', $(e.target).attr('href'));
+});
+var activeTab = localStorage.getItem('activeTab');
+if(activeTab){
+    $('#myTab a[href="' + activeTab + '"]').tab('show');
+}
